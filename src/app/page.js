4a92ffd683541c408bb559b7b720807b5e13f4a4
kumar1397 <sahuiswar007@outlook.com>
2024-11-17 22:09:46 +0530

    3

--- src/app/page.js
+++ src/app/page.js
@@ -37,7 +37,7 @@ export default function HomePage() {
         <>
             <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-slate-800 drop-shadow-xl">
                 <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
-                    My FULL STACK Blog App With Next.js
+                    Contact Management App
                 </h1>
             </div>
 
